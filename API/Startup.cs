4556d55cfc27af468aa6c82220486e37c3f3aa41
Dexxem <rsalolainen@gmail.com>
2020-12-04 19:28:11 +0200

    stage2.5

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore;
+
 
 namespace API
 {
